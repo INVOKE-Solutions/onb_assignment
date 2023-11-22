4ecee87e79722ff8b869fa7b1a067ae4df45395a
fahmizainal17 <fahmizainal9@gmail.com>
2023-11-22 17:08:47 +0800

    feat:add files

---
+++ calculator.app/calculator.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    """Addition feature"""
+    return a + b
+
+def subtract(a, b):
+    """Subtraction feature"""
+    return a - b
+
+def multiply(a, b):
+    """Multiplication feature"""
+    return a * b
+
+def divide(a, b):
+    """Division feature"""
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
---
+++ calculator.app/main_ui.py
@@ -0,0 +1,13 @@
+# main_ui.py
+import streamlit as st
+from calculator import add, subtract, multiply, divide
+
+# Streamlit code for UI
+# ...
+
+# You can call the calculator functions in this file as needed
+# calculator.py
+
+
+x = divide(31,2)
+st.write(x)
